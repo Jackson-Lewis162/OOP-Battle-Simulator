688af7d2ff5e36757dfcf9aff6cf5f55f06163b5
Jackson-Lewis162 <jackson.lewis@ascte.org>
2026-09-14 10:37:59 -0500

    AddedGoblin

--- game.py
+++ game.py
@@ -13,6 +13,10 @@ def main():
     goblin = Goblin("Gribble")
 
     print(f"{goblin.name} enters the arena with {goblin.health} health.")
+
+    ribble = Goblin("ribble")
+
+    print(f"{ribble.name} enters the arena with {ribble.health} health.")
     print("But no hero has answered the call... yet.")
 
 
