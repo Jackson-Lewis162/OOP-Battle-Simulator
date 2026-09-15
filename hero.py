9166e3899474738e535635cea6bce4ecb351d54e
Jackson-Lewis162 <jackson.lewis@ascte.org>
2026-09-15 10:51:01 -0500

    Added hero battle

--- hero.py
+++ hero.py
@@ -1,4 +1,18 @@
+import random
 class Hero:
     """The hero blueprint will be implemented later in the project."""
+    def __init__(self, name):
+        self.name = name
+        self.health = 120
+        self.attack_power = 20
+    def attack(self):
+        return random.radient(1, self.attack_power)
 
-    pass
+    def take_damage(self, damage):
+        self.health = self.health - damage
+        if self.health < 0:
+            self.health = 0
+
+    def is_alive(self):
+        return self.health > 0
+
